39f3aed98806fda6a74e1b015dbcbfb96e2a2725
RachelBep <151029551+RachelBep@users.noreply.github.com>
2024-11-27 15:48:35 +0000

    initial commit

---
+++ src/main/kotlin/models/Card.kt
@@ -0,0 +1,10 @@
+package ie.setu.models
+
+class Card (
+    var cardName: String,
+    var hp: Int = 0,
+    var dmg: Int = 0,
+    var tribe: String,
+    var cost: Int = 0,
+    var costType: String
+)
---
+++ src/main/kotlin/models/Sigil.kt
@@ -0,0 +1,7 @@
+package ie.setu.models
+
+class Sigil(
+    var sigilName: String,
+    var sigilDescription: String,
+    var power: Int
+    )
